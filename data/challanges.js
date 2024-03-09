1dd9f9fad4e17e6faa114ff9c50f59861d55417f
Iulian Poenaru <poenaruiulian2003@gmail.com>
2024-03-10 01:08:51 +0200

    Cleanup images and some files

--- data/challanges.js
+++ data/challanges.js
@@ -2,10 +2,10 @@ export const challanges = [
   {
     title: "Recycle is business!",
     description:
-      "Recycle 10 object in the next 10 minutes. This objects can be either bottles, cans or paper boxes.",
+      "Recycle 3 object in the next minute. This objects can be either plastic, aluminium or paper.",
     instructions: [
       "Start the timer ⏳",
-      "Go and scan 10 objects in the next 10 minutes 📸",
+      "Go and scan 3 objects in the next minute 📸",
       "If you succeed the challenge you will be notified to redeem your prize 🏆",
       "If you fail the challenge you will get 1 coin for each object scanned ",
     ],
