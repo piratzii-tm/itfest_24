b6b40d45af9964dfe44d32e8ca9a3ca21bf3d69d
Iulian Poenaru <poenaruiulian2003@gmail.com>
2024-03-16 14:53:38 +0200

    Reverted deleted data

---
+++ data/challanges.js
@@ -0,0 +1,15 @@
+export const challanges = [
+  {
+    title: "Recycle is business!",
+    description:
+      "Recycle 3 object in the next minute. This objects can be either plastic, aluminium or paper.",
+    instructions: [
+      "Start the timer ⏳",
+      "Go and scan 3 objects in the next minute 📸",
+      "If you succeed the challenge you will be notified to redeem your prize 🏆",
+      "If you fail the challenge you will get 1 coin for each object scanned ",
+    ],
+    prize: 15,
+    minimumScans: 3,
+  },
+];
